74486c956362377b948a41b8bcec9fe2ab4d0292
Emmett Martin <emmettmartin273@gmail.com>
2022-08-06 21:52:55 -0600

    Update 0.1.4
    Added resize window method; renamed window.

--- main/main.py
+++ main/main.py
@@ -6,6 +6,8 @@ Made July 10th, author: Emmett of Hats Off Studios.
 
 With help from de333r.
 
+v0.1.4
+
 ---------------------------------------------------
 
 Main file.
@@ -18,12 +20,34 @@ import random as rand
 import time as ti
 
 
-print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-print("^^^^^^^^^^^^^^^^^^^^^ Main Program Start! ^^^^^^^^^^^^^^^^^^^^^")
-print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+print("********************")
+print("*Main Program Start*")
+print("********************")
 
 
-new_window = tk.Tk()
+main_window = tk.Tk()
+
+
+def resize_window(window_name, x, y, multiplier):
+
+    if multiplier == 0:
+        kill_window(main_window)
+        
+        print("Error: Cannot make window with multiplier value 0\nIntentionalError")
+
+        quit()
+
+    try:
+        mod_dimensions_x = x * multiplier
+        mod_dimensions_y = y * multiplier
+
+        geometry_string = str(mod_dimensions_x)+"x"+str(mod_dimensions_y)
+
+        window_name.geometry(geometry_string)
+    except tk.TclError:
+
+        pass
+
 
 def kill_window(window_name):
     window_name.destroy()
@@ -33,8 +57,10 @@ def key_pressed(event):
     key = str.lower(event.char)
 
     if key == chr(27):
-        kill_window(new_window)
+        kill_window(main_window)
 
-new_window.bind("<Key>", key_pressed)
+resize_window(main_window, 189, 63, 8)
 
-new_window.mainloop()
+main_window.bind("<Key>", key_pressed)
+
+main_window.mainloop()
