ba4a786547e27d2feaf46071742176fbb55b97ea
Emmett <emmettmartin273@gmail.com>
2023-04-10 13:54:14 -0600

    Made more changes to player file.

--- main/movement.py
+++ main/movement.py
@@ -1,2 +1,15 @@
-import player
+# import player
 import collision
+
+class Mover:
+    def __init__(self) -> None:
+        pass
+
+    def move_left(self):
+        print("left")
+
+    def move_right(self):
+        pass
+
+    def jump(self):
+        pass
--- main/player.py
+++ main/player.py
@@ -1,5 +1,7 @@
 import movement
 
+move = movement.Mover()
+
 class Player:
     def __init__(self, curr_pos_x, curr_pos_y, prev_x, prev_y, starting_x, starting_y) -> None:
         self.curr_pos_x = curr_pos_x
@@ -8,3 +10,6 @@ class Player:
         self.prev_y = prev_y
         self.starting_x = starting_x
         self.starting_y = starting_y
+    
+    def player_move_left(self):
+        move.move_left()
