035d4cf401f9b52e0e39bc2ce85d19c64ca0d15e
Emmett <emmettmartin273@gmail.com>
2023-04-10 16:32:25 -0600

    Fixed small problem with the returns.

--- main/player.py
+++ main/player.py
@@ -2,7 +2,7 @@ import time
 
 class Player:
     def __init__(self, curr_pos_x, curr_pos_y, prev_x, prev_y, starting_x, starting_y, sprite_state) -> None:
-        
+
         self.curr_pos_x = curr_pos_x
         self.curr_pos_y = curr_pos_y
         self.prev_x = prev_x
@@ -30,7 +30,9 @@ class Player:
         for i in range(30):
             self.curr_pos_y -= 1
             time.sleep(0.1)
+            return self.curr_pos_y
 
         for i in range(30):
             self.curr_pos_y += 1
             time.sleep(0.1)
+            return self.curr_pos_y
